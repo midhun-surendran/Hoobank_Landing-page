a3396975321b224f3139c3e41da9d2719a14e5c5
Midhun Surendran <midhunsurendran4558@gmail.com>
2022-08-29 11:26:32 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Bussiness,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
+// main layout of landing page
+
+import styles from "./style";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary w-full overflow-hidden">
+      {/* navbar */}
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      {/* hero-secton */}
+
+      <div className={`bg-primary ${styles.flexStart} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      {/* other components */}
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Bussiness />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
